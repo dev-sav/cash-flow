38e7ef05420c6326a5442c476bbe6b41f729cc50
dev-sav <138636901+dev-sav@users.noreply.github.com>
2025-02-13 20:31:38 +0800

    Add backend

--- back-end/server.js
+++ back-end/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const mysql = require("mysql");
 const cors = require("cors");
 const pdfParse = require("pdf-parse");
@@ -7,222 +8,9 @@ const fileupload = require("express-fileupload");
 const app = express();
 app.use(cors());
 
-app.get("/", (res) => {
+app.get("/", (req, res) => {
   return res.json("from backend bitch");
 });
-
-const trimFluffs = (arr) => {
-  const anchor = ["BEGINNING", "BALANCE", "AMOUNT", "THIS"];
-  let start;
-  let end = arr.length; // only applicable on the last page
-  for (let i = 0; i < arr.length - 1; i++) {
-    if ([anchor[0], anchor[2]].includes(arr[i]) && arr[i + 1] === anchor[1]) {
-      if (arr[i] === anchor[0]) start = i + 3;
-      else start = i + 2;
-    }
-    if (arr[i] === anchor[1] && arr[i + 1] === anchor[3]) end = i;
-  }
-  const cleanedPage = arr.slice(start, end);
-
-  return cleanedPage;
-};
-
-const getWords = (text) => {
-  const words = text.split(" ");
-  const trimEnd = words.slice(0, words.length - 1);
-  return trimEnd;
-};
-
-function chopItems(data) {
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  const result = [];
-  let currentChunk = [];
-
-  data.forEach((item) => {
-    if (months.includes(item)) {
-      if (currentChunk.length > 0) {
-        result.push(currentChunk);
-      }
-      currentChunk = [item];
-    } else {
-      currentChunk.push(item);
-    }
-  });
-
-  if (currentChunk.length > 0) {
-    result.push(currentChunk);
-  }
-
-  return result;
-}
-
-const isRef = (str) => {
-  const refRegex = /^\d{3,4}$/;
-  return refRegex.test(str);
-};
-
-const buildString = (obj, arr, startDesc, endDesc, stardDeets, endDeets) => {
-  const strDesc = arr.slice(startDesc, endDesc).join(" ");
-  const strDeets = arr.slice(stardDeets, endDeets).join(" ");
-  obj.description = strDesc;
-  obj.details = strDeets;
-
-  return obj;
-};
-/*
-[
-    "Partner",
-    "Merchant",
-    "Cash",
-    "In",
-    "0060",
-    "TO:",
-    "SHOPEEPAY",
-    "PHILI.....XXXXX43486"
-    ]
-    */
-const groupData = (obj, arr) => {
-  let newObj;
-
-  const match = arr.find((word) => isRef(word));
-  if (match) {
-    const i = arr.indexOf(match);
-    if (isRef(arr[i + 1]))
-      newObj = buildString(obj, arr, 0, i + 2, i + 3, arr.length);
-    else newObj = buildString(obj, arr, 0, i + 1, i + 1, arr.length);
-  } else {
-    const floor = Math.floor(arr.length / 2);
-    const ceil = Math.ceil(arr.length / 2);
-    newObj = buildString(obj, arr, 0, floor, ceil, arr.length);
-  }
-
-  return newObj;
-};
-
-/* 
-    [
-        [
-        "Oct",
-        "14",
-        "Partner",
-        "Merchant",
-        "Cash",
-        "In",
-        "0060",
-        "TO:",
-        "SHOPEEPAY",
-        "PHILI.....XXXXX43486",
-        "108.00",
-        "15,402.51"
-        ],
-        ....,
-        ....,
-     ] 
-*/
-
-const categorizeData = (arr) => {
-  const result = [];
-
-  arr.forEach((item) => {
-    let current = {};
-    console.log(item[0]);
-    current.date = `${item[0]}  ${item[1]}`;
-    current.balance = `${item[item.length - 1]}`;
-    current.amount = `${item[item.length - 2]}`;
-    const newObj = groupData(current, item.slice(2, item.length - 2));
-    result.push(newObj);
-  });
-
-  /*
-    [
-        "Oct",
-        "14",
-        "Partner",
-        "Merchant",
-        "Cash",
-        "In",
-        "0060",
-        "TO:",
-        "SHOPEEPAY",
-        "PHILI.....XXXXX43486",
-        "108.00",
-        "15,402.51"
-        ],
-    */
-
-  return result;
-};
-
-const cleanPages = (text) => {
-  // remove invalid pages
-  const lines = text.split("\n");
-  const sliced = lines.slice(6, lines.length - 2);
-  const filtered = sliced.filter((_, index) => index % 2 === 0);
-
-  let cleanedData = [];
-
-  for (let value of filtered) {
-    const words = getWords(value);
-    const trimmed = trimFluffs(words);
-    const chopped = chopItems(trimmed);
-    /* 
-    [
-        [
-        "Oct",
-        "14",
-        "Partner",
-        "Merchant",
-        "Cash",
-        "In",
-        "0060",
-        "TO:",
-        "SHOPEEPAY",
-        "PHILI.....XXXXX43486",
-        "108.00",
-        "15,402.51"
-        ],
-        ....,
-        ....,
-     ] 
-    */
-    cleanedData = [...cleanedData, ...chopped];
-  }
-
-  const categorizedData = categorizeData(cleanedData);
-
-  return categorizedData;
-};
-
-const processPdfText = (text) => {
-  const cleanedPages = cleanPages(text);
-
-  return cleanedPages;
-
-  /*  // Find the index of the line containing the searchString
-    const startIndex = lines.findIndex((line) => line.includes("Page3of4"));
-    const endIndex = lines.findIndex((line) => line.includes("Page4of4"));
-  
-    // If the searchString is not found or there aren't enough lines after it, return an empty array
-    if (startIndex === -1 || startIndex + 3 >= lines.length) {
-      return [];
-    }
-  
-    // Extract the specified number of lines after the found line
-    return lines.slice(startIndex + 4, endIndex - 2); */
-};
 
 app.use(fileupload());
 app.post("/extract-text", (req, res) => {
@@ -248,11 +36,68 @@ app.post("/extract-text", (req, res) => {
       },
     };
     pdfParse(req.files.pdfFile, options).then((result) => {
-      res.send(processPdfText(result.text));
+      res.send(result.text);
     });
   }
 });
 
-app.listen(8081, () => {
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '#Chuckbart05',
+  database: "cash_flow"
+});
+// Connect to MySQL
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL: ' + err.stack);
+    return;
+  }
+  console.log('Connected to MySQL as ID ' + db.threadId);
+});
+
+// get transactions
+app.get('/transactions', (req, res) => {
+  db.query('SELECT * FROM transactions', (err, results) => {
+    if (err) {
+      console.error('Error executing query: ' + err.stack);
+      res.status(500).send('Error fetching users');
+      return;
+    }
+    res.json(results);
+  });
+});
+
+// Create a new user
+app.post('/transactions', (req, res) => {
+  const { date, description, amount, balance, details, remarks } = req.body;
+  db.query('INSERT INTO transactions (date, description, amount, balance, details, remarks) VALUES (? , ?, ?, ?, ?,?);', [date, description, amount, balance,details, remarks], (err, result) => {
+    if (err) {
+      console.error('Error executing query: ' + err.stack);
+      res.status(400).send('Error creating user');
+      return;
+    }
+    res.status(201).send('User created successfully');
+  });
+});
+
+
+// Delete a user
+app.delete('/transactions/:id', (req, res) => {
+  const id = req.params.id;
+  db.query('DELETE FROM transactions WHERE id = ?', [id], (err, result) => {
+    if (err) {
+      console.error('Error executing query: ' + err.stack);
+      res.status(400).send('Error deleting user');
+      return;
+    }
+    res.send('User deleted successfully');
+  });
+});
+
+app.listen(8080, () => {
+
   console.log("listening... bitch!");
 });
--- front-end/src/utils/PdfParser.util.ts
+++ front-end/src/utils/PdfParser.util.ts
@@ -1,53 +1,212 @@
-function trimFront(arr: string[], anchor1: string, anchor2: string) {
+const trimFluffs = (arr) => {
+  const anchor = ["BEGINNING", "BALANCE", "AMOUNT", "THIS"];
   let start;
+  let end = arr.length; // only applicable on the last page
   for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] === anchor1 && arr[i + 1] === anchor2) {
-      start = i + 2;
+    if ([anchor[0], anchor[2]].includes(arr[i]) && arr[i + 1] === anchor[1]) {
+      if (arr[i] === anchor[0]) start = i + 3;
+      else start = i + 2;
     }
+    if (arr[i] === anchor[1] && arr[i + 1] === anchor[3]) end = i;
   }
-  const cleanedPage = arr.slice(start, arr.length);
+  const cleanedPage = arr.slice(start, end);
+
   return cleanedPage;
-}
+};
 
-function getWords(text: string) {
+const getWords = (text) => {
   const words = text.split(" ");
-  return words;
-}
+  const trimEnd = words.slice(0, words.length - 1);
+  return trimEnd;
+};
 
-function cleanPages(text: string) {
-  // remove invalid pages
-  const lines = text.split("\n");
-  const filtered = lines.filter((element, index) => index % 2 === 0);
-  const pages = filtered.slice(3, lines.length - 1);
+function chopItems(data) {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const result = [];
+  let currentChunk = [];
 
-  return pages;
-
-  let trimmedPages = [];
-  pages.forEach((page) => {
-    const words = getWords(page);
-    const trimmedFront = trimFront(words, "BEGINNING", "BALANCE");
-    trimmedPages.push(trimmedFront);
+  data.forEach((item) => {
+    if (months.includes(item)) {
+      if (currentChunk.length > 0) {
+        result.push(currentChunk);
+      }
+      currentChunk = [item];
+    } else {
+      currentChunk.push(item);
+    }
   });
 
-  return pages;
+  if (currentChunk.length > 0) {
+    result.push(currentChunk);
+  }
+
+  return result;
 }
 
-function processPdfText(text: string) {
+const isRef = (str) => {
+  const refRegex = /^\d{3,4}$/;
+  return refRegex.test(str);
+};
+
+const buildString = (obj, arr, startDesc, endDesc, stardDeets, endDeets) => {
+  const strDesc = arr.slice(startDesc, endDesc).join(" ");
+  const strDeets = arr.slice(stardDeets, endDeets).join(" ");
+  obj.description = strDesc;
+  obj.details = strDeets;
+
+  return obj;
+};
+/*
+[
+    "Partner",
+    "Merchant",
+    "Cash",
+    "In",
+    "0060",
+    "TO:",
+    "SHOPEEPAY",
+    "PHILI.....XXXXX43486"
+    ]
+    */
+const groupData = (obj, arr) => {
+  let newObj;
+
+  const match = arr.find((word) => isRef(word));
+  if (match) {
+    const i = arr.indexOf(match);
+    if (isRef(arr[i + 1]))
+      newObj = buildString(obj, arr, 0, i + 2, i + 3, arr.length);
+    else newObj = buildString(obj, arr, 0, i + 1, i + 1, arr.length);
+  } else {
+    const floor = Math.floor(arr.length / 2);
+    const ceil = Math.ceil(arr.length / 2);
+    newObj = buildString(obj, arr, 0, floor, ceil, arr.length);
+  }
+
+  return newObj;
+};
+
+/* 
+    [
+        [
+        "Oct",
+        "14",
+        "Partner",
+        "Merchant",
+        "Cash",
+        "In",
+        "0060",
+        "TO:",
+        "SHOPEEPAY",
+        "PHILI.....XXXXX43486",
+        "108.00",
+        "15,402.51"
+        ],
+        ....,
+        ....,
+     ] 
+*/
+
+const categorizeData = (arr) => {
+  const result = [];
+
+  arr.forEach((item) => {
+    let current = {};
+    console.log(item[0]);
+    current.date = `${item[0]}  ${item[1]}`;
+    current.balance = `${item[item.length - 1]}`;
+    current.amount = `${item[item.length - 2]}`;
+    const newObj = groupData(current, item.slice(2, item.length - 2));
+    result.push(newObj);
+  });
+
+  /*
+    [
+        "Oct",
+        "14",
+        "Partner",
+        "Merchant",
+        "Cash",
+        "In",
+        "0060",
+        "TO:",
+        "SHOPEEPAY",
+        "PHILI.....XXXXX43486",
+        "108.00",
+        "15,402.51"
+        ],
+    */
+
+  return result;
+};
+
+const cleanPages = (text) => {
+  // remove invalid pages
+  const lines = text.split("\n");
+  const sliced = lines.slice(6, lines.length - 2);
+  const filtered = sliced.filter((_, index) => index % 2 === 0);
+
+  let cleanedData = [];
+
+  for (let value of filtered) {
+    const words = getWords(value);
+    const trimmed = trimFluffs(words);
+    const chopped = chopItems(trimmed);
+    /* 
+    [
+        [
+        "Oct",
+        "14",
+        "Partner",
+        "Merchant",
+        "Cash",
+        "In",
+        "0060",
+        "TO:",
+        "SHOPEEPAY",
+        "PHILI.....XXXXX43486",
+        "108.00",
+        "15,402.51"
+        ],
+        ....,
+        ....,
+     ] 
+    */
+    cleanedData = [...cleanedData, ...chopped];
+  }
+
+  const categorizedData = categorizeData(cleanedData);
+
+  return categorizedData;
+};
+
+const processPdfText = (text) => {
   const cleanedPages = cleanPages(text);
 
   return cleanedPages;
 
   /*  // Find the index of the line containing the searchString
-  const startIndex = lines.findIndex((line) => line.includes("Page3of4"));
-  const endIndex = lines.findIndex((line) => line.includes("Page4of4"));
-
-  // If the searchString is not found or there aren't enough lines after it, return an empty array
-  if (startIndex === -1 || startIndex + 3 >= lines.length) {
-    return [];
-  }
-
-  // Extract the specified number of lines after the found line
-  return lines.slice(startIndex + 4, endIndex - 2); */
-}
-
-export default processPdfText;
+    const startIndex = lines.findIndex((line) => line.includes("Page3of4"));
+    const endIndex = lines.findIndex((line) => line.includes("Page4of4"));
+  
+    // If the searchString is not found or there aren't enough lines after it, return an empty array
+    if (startIndex === -1 || startIndex + 3 >= lines.length) {
+      return [];
+    }
+  
+    // Extract the specified number of lines after the found line
+    return lines.slice(startIndex + 4, endIndex - 2); */
+};
